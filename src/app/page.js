26bb7da5b928595dde6f87874fbd16c69d3c955d
lloydchang <lloydchang@gmail.com>
2024-08-13 01:16:28 -0700

    feat(page.js): add
    5. Stripe Integration:
    
    This function handles the Stripe checkout process when a user selects the Pro plan.
    
    Next, let’s move on to the Stripe result page.

--- src/app/page.js
+++ src/app/page.js
@@ -42,3 +42,19 @@
   </Grid>
 </Box>
 
+const handleSubmit = async () => {
+  const checkoutSession = await fetch('/api/checkout_sessions', {
+    method: 'POST',
+    headers: { origin: 'http://localhost:3000' },
+  })
+  const checkoutSessionJson = await checkoutSession.json()
+
+  const stripe = await getStripe()
+  const {error} = await stripe.redirectToCheckout({
+    sessionId: checkoutSessionJson.id,
+  })
+
+  if (error) {
+    console.warn(error.message)
+  }
+}
