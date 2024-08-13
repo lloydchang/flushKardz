2ee2582e8ac08aa3f1bc16e3b51edfc0fb1def2e
lloydchang <lloydchang@gmail.com>
2024-08-13 07:12:30 -0700

    feat(middleware.js): add
    https://dashboard.clerk.com/

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  // Specify your custom matcher here if needed
-});
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
